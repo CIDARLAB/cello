1d025de95865bc6ef4e480003dd493214b64c210
Tim Jones <tjones01@gmail.com>
2017-08-14 15:26:13 -0400

    instantiate part library, fix indentation, organize imports

--- src/main/java/org/cellocad/adaptors/synbiohubadaptor/SynBioHubAdaptor.java
+++ src/main/java/org/cellocad/adaptors/synbiohubadaptor/SynBioHubAdaptor.java
@@ -3,55 +3,58 @@ package org.cellocad.adaptors.synbiohubadaptor;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.HashSet;
 
+import java.lang.UnsupportedOperationException;
+import org.cellocad.MIT.dnacompiler.GateLibrary;
 import org.cellocad.MIT.dnacompiler.Part;
 import org.cellocad.MIT.dnacompiler.PartLibrary;
-import org.cellocad.MIT.dnacompiler.Gate;
-import org.cellocad.MIT.dnacompiler.GateLibrary;
-import org.sbolstandard.core2.SBOLDocument;
 import org.sbolstandard.core2.ComponentDefinition;
+import org.sbolstandard.core2.SBOLDocument;
 import org.sbolstandard.core2.SequenceOntology;
 import org.synbiohub.frontend.SynBioHubFrontend;
 
+/**
+ * An adaptor to build the parts library from SynBioHub.
+ *
+ * @author: Tim Jones
+ */
 public class SynBioHubAdaptor {
-    private SBOLDocument celloSBOL;
+	private SBOLDocument celloSBOL;
 	private PartLibrary partLibrary;
 	private GateLibrary gateLibrary;
 
 	public SynBioHubAdaptor() {
+		partLibrary = new PartLibrary();
 		fetchCelloSBOL();
 		createLibraries();
 	}
 
-    private void fetchCelloSBOL() {
-        SynBioHubFrontend sbh = new SynBioHubFrontend("https://synbiohub.programmingbiology.org");
+	private void fetchCelloSBOL() {
+		SynBioHubFrontend sbh = new SynBioHubFrontend("https://synbiohub.programmingbiology.org");
 
-        URI u = URI.create("https://synbiohub.programmingbiology.org/public/Cello_Parts/Cello_Parts_collection/1");
+		URI u = URI.create("https://synbiohub.programmingbiology.org/public/Cello_Parts/Cello_Parts_collection/1");
 
-    	try {
+		try {
 			celloSBOL = sbh.getSBOL(u);
-    	} catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-	
+	}
+
 	private void createLibraries() {
-        HashMap<String,Part> allParts = new HashMap();
-		
-    	Set<ComponentDefinition> celloCD = celloSBOL.getComponentDefinitions();
-    	
-    	for (ComponentDefinition cd : celloCD) {
+		HashMap<String,Part> allParts = new HashMap();
+
+		Set<ComponentDefinition> celloCD = celloSBOL.getComponentDefinitions();
+
+		for (ComponentDefinition cd : celloCD) {
 			// i dont know why there would be more than one type per part
 			// in the cello parts there is one type per part, so just grab the first type below
 			URI type = cd.getTypes().iterator().next();
 			if (type.equals(URI.create("http://www.biopax.org/release/biopax-level3.owl#DnaRegion"))) {
 				URI role = cd.getRoles().iterator().next();
 				if (role.equals(SequenceOntology.ENGINEERED_REGION)) { // if the CD is a gate
-					//GateLibrary gate_library = new GateLibrary(n_inputs, n_outputs);
-
+					// GateLibrary gate_library = new GateLibrary(n_inputs, n_outputs);
 					//read gates and create gate objects
-
 				} else { // otherwise it's a part
 					String name = cd.getName();
 					String roleString = "NOT_SET";
@@ -69,26 +72,26 @@ public class SynBioHubAdaptor {
 						roleString = "scar";
 
 					String seq = cd.getSequences().iterator().next().getElements();
-		
+
 					allParts.put(name, new Part(name, roleString, seq));
 				}
 			}
-    	}
-        partLibrary.set_ALL_PARTS(allParts);
-    }
+		}
+		partLibrary.set_ALL_PARTS(allParts);
+	}
 
 	/**
-	 * @return the celloSBOL
+	 * @return the SBOLDocument containing the parts library
 	 */
 	public SBOLDocument getCelloSBOL() {
 		if (celloSBOL == null)
 			fetchCelloSBOL();
-		
+
 		return celloSBOL;
 	}
 
 	/**
-	 * @return the partLibrary
+	 * @return the PartLibrary object that was build from the SBOL
 	 */
 	public PartLibrary getPartLibrary() {
 		return partLibrary;
@@ -98,6 +101,9 @@ public class SynBioHubAdaptor {
 	 * @return the gateLibrary
 	 */
 	public GateLibrary getGateLibrary() {
+		if ( gateLibrary == null ) {
+			throw(new UnsupportedOperationException("Not yet implemented."));
+		}
 		return gateLibrary;
 	}
 }
--- src/test/java/SynBioHubTest.java
+++ src/test/java/SynBioHubTest.java
@@ -1,34 +1,42 @@
+import static org.junit.Assert.assertTrue;
 
-
-import static org.junit.Assert.*;
-import org.cellocad.adaptors.synbiohubadaptor.*;
-import org.cellocad.adaptors.ucfadaptor.*;
-import org.cellocad.MIT.dnacompiler.*;
 import java.util.HashMap;
 import java.util.Set;
 
+import org.cellocad.MIT.dnacompiler.Part;
+import org.cellocad.MIT.dnacompiler.PartLibrary;
+import org.cellocad.MIT.dnacompiler.UCF;
+import org.cellocad.adaptors.synbiohubadaptor.SynBioHubAdaptor;
+import org.cellocad.adaptors.ucfadaptor.UCFAdaptor;
+import org.cellocad.adaptors.ucfadaptor.UCFReader;
 import org.junit.Test;
 
+/**
+ * Compare the output (part names) of the UCF-generated parts library
+ * to the output of the SynBioHub-generated parts library.
+ *
+ * @author: Tim Jones
+ */
 public class SynBioHubTest {
 
-    @Test
-    public void test() {
-	UCFAdaptor ucfAdaptor = new UCFAdaptor();
-	UCFReader ucfReader = new UCFReader();
-	UCF ucf = ucfReader.readAllCollections("resources/UCF/Eco1C1G1T0.UCF.json");
-	PartLibrary partLib = ucfAdaptor.createPartLibrary(ucf);
-	HashMap<String,Part> ucfParts = partLib.get_ALL_PARTS();
-	Set<String> ucfPartNames = ucfParts.keySet();
-								
-	SynBioHubAdaptor sbhAdaptor = new SynBioHubAdaptor();
-	HashMap<String,Part> sbhParts = sbhAdaptor.createPartLibrary().get_ALL_PARTS();
-	Set<String> sbhPartNames = sbhParts.keySet();
+	@Test
+	public void test() {
+		UCFAdaptor ucfAdaptor = new UCFAdaptor();
+		UCFReader ucfReader = new UCFReader();
+		UCF ucf = ucfReader.readAllCollections("resources/UCF/Eco1C1G1T0.UCF.json");
+		PartLibrary partLib = ucfAdaptor.createPartLibrary(ucf);
+		HashMap<String,Part> ucfParts = partLib.get_ALL_PARTS();
+		Set<String> ucfPartNames = ucfParts.keySet();
 
-	for (String name : ucfPartNames) {
-	    assertTrue(name + " in the UCF is missing from SynBioHub", sbhPartNames.contains(name));
+		SynBioHubAdaptor sbhAdaptor = new SynBioHubAdaptor();
+		HashMap<String,Part> sbhParts = sbhAdaptor.getPartLibrary().get_ALL_PARTS();
+		Set<String> sbhPartNames = sbhParts.keySet();
+
+		for (String name : ucfPartNames) {
+			assertTrue(name + " in the UCF is missing from SynBioHub", sbhPartNames.contains(name));
+		}
+		for (String name : sbhPartNames) {
+			assertTrue(name + " in SynBioHub is missing from the UCF", ucfPartNames.contains(name));
+		}
 	}
-	for (String name : sbhPartNames) {
-	    assertTrue(name + " in SynBioHub is missing from the UCF", ucfPartNames.contains(name));
-	}
-    }
 }
